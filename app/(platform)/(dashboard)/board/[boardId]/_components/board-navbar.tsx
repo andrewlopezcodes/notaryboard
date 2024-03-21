7b978f36098b3c93ea35a58491ef38b2aa3401e7
Andrew Lopez <andrewlopezcodes@gmail.com>
2024-03-21 10:04:43 -0400

    add feature delete board

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -2,6 +2,7 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { Board } from "@prisma/client";
 import { BoardTitleForm } from "./board-title-form";
+import { BoardOptions } from "./board-options";
 
 
 interface BoardNavbarProps {
@@ -28,6 +29,11 @@ export const BoardNavbar = async ({
         text-white"
     >
       <BoardTitleForm data={data} />
+      <div
+        className="ml-auto">
+          <BoardOptions 
+          id={data.id}/>
+        </div>
     </div>
   );
 };
